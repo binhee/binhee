22e827db33f528a12297568d21d5c17e42dc7082
binhee <bh7149@naver.com>
2023-12-28 09:28:32 +0900

    Character..........

--- Assets/Script/PlayerContoller/Movement.cs
+++ Assets/Script/PlayerContoller/Movement.cs
@@ -4,36 +4,46 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    // CharacterController 스크립트 참조 변수
     private CharacterController _controller;
-
+    // 이동 방향
     private Vector2 _movementDirection = Vector2.zero;
+    // Rigidbody2D 컴포넌트 참조 변수
     private Rigidbody2D _rigidbody;
 
     private void Awake()
     {
+        // CharacterController 컴포넌트 가져오기
         _controller = GetComponent<CharacterController>();
+        // Rigidbody2D 컴포넌트 가져오기
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     private void Start()
     {
+        // 이동 이벤트에 Move 메서드 등록
         _controller.OnMoveEvent += Move;
     }
 
     private void FixedUpdate()
     {
+        // 이동 적용 메서드 호출
         ApplyMovment(_movementDirection);
     }
 
     private void Move(Vector2 direction)
     {
+        // 이동 이벤트를 통해 받은 방향으로 설정하는 메서드
         _movementDirection = direction;
     }
 
+    // 실제 이동을 적용하는 메서드
     private void ApplyMovment(Vector2 direction)
     {
+        // 이동 방향에 상수를 곱하여 속도 조절
         direction = direction * 5;
 
+        // Rigidbody2D의 속도를 설정하여 이동 적용
         _rigidbody.velocity = direction;
     }
 }
--- Assets/Script/PlayerContoller/PlayerInputController.cs
+++ Assets/Script/PlayerContoller/PlayerInputController.cs
@@ -3,32 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerInputController : MonoBehaviour
+public class PlayerInputController : CharacterController
 {
     private Camera _camera;
 
     private void Awake()
     {
-        // Awake 메서드에서 메인 카메라를 가져옵니다.
+        // Awake 메서드에서 메인 카메라를 가져옴
         _camera = Camera.main;
     }
 
     public void OnMove(InputValue value)
     {
-        // 이동 입력을 받아서 정규화한 벡터로 변환합니다.
+        // 이동 입력을 받아서 정규화한 벡터로 변환
         Vector2 moveInput = value.Get<Vector2>().normalized;
-        // 이동 이벤트 호출합니다.
+        // 이동 이벤트 호출
         CallMoveEvent(moveInput);
     }
 
     public void OnLook(InputValue value)
     {
-        // 시선 이동 입력을 받아서 화면 좌표를 월드 좌표로 변환합니다.
+        // 마우스 시선 이동 입력을 받아서 화면 좌표를 월드 좌표로 변환
         Vector2 newAim = value.Get<Vector2>();
         Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
         newAim = (worldPos - (Vector2)transform.position).normalized;
 
-        // 시선 벡터의 크기가 0.9 이상이면 시선 이벤트 호출합니다.
+        // 시선 벡터의 크기가 0.9 이상이면 시선 이벤트 호출
         if (newAim.magnitude >= .9f)
         {
             CallLookEvent(newAim);
@@ -37,19 +37,7 @@ public class PlayerInputController : MonoBehaviour
 
     public void OnFire(InputValue value)
     {
-        // 발사 입력을 처리하는 메서드입니다.
-        // 현재는 빈 메서드입니다. 발사 로직을 여기에 구현하세요.
-    }
-
-    // 이동 이벤트 호출을 담당하는 메서드
-    private void CallMoveEvent(Vector2 moveInput)
-    {
-        // 이동 이벤트 호출 로직을 여기에 추가하세요.
-    }
-
-    // 시선 이벤트 호출을 담당하는 메서드
-    private void CallLookEvent(Vector2 aimDirection)
-    {
-        // 시선 이벤트 호출 로직을 여기에 추가하세요.
-    }
+        // 발사 입력을 처리하는 메서드
+        // 현재는 빈 메서드 발사 로직을 여기에 구현하세요.
+    }    
 }
